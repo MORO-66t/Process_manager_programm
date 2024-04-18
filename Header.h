febc48a9226db32e15557e9d2b634787ad5dec86
MORO-66t <scwtud@gmail.com>
2024-04-18 08:34:26 +0200

    Add structure of the program

---
+++ Header.h
@@ -0,0 +1,49 @@
+#ifndef MIAN_H
+#define MAIN_H
+
+#include <stdin.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h> // for sleep fun
+//#include <unistd.h> // works only in linux systems 
+//#include <sys/wait.h> // they does not work on Windows
+
+
+
+
+
+/*MAIN FUNCTIONS*/
+void Menu();
+void choose(int choice);
+
+
+/* List all the processes in the system.*/
+
+
+
+/*List all the processes grouped by user*/
+
+
+
+/* Display process ID of all the processes*/
+
+
+
+/*Run/stop a specific process*/
+
+
+
+/* Send specific signals to specific process.*/
+
+
+
+
+
+
+
+
+
+
+
+#endif
